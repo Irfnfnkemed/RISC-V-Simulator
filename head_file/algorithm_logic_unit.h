4600f91bb942d4bb20d044e73fe6695f31b5cd68
Irfnfnkemed <Irfnfnkemed@sjtu.edu.cn>
2023-06-23 16:13:02 +0800

    rename the ALU module

---
+++ head_file/algorithm_logic_unit.h
@@ -0,0 +1,22 @@
+#ifndef RISC_V_SIMULATOR_ALGORITHM_LOGIC_UNIT_H
+#define RISC_V_SIMULATOR_ALGORITHM_LOGIC_UNIT_H
+
+#include "../head_file/tool.h"
+
+class algorithm_logic_unit {
+private:
+    enum INSTRUCTION {
+        ADDI, SLTI, SLTIU, XORI, ORI, ANDI, SLLI, SRLI, SRAI,
+        ADD, SUB, SLL, SLT, SLTU, XOR, SRL, SRA, OR, AND
+    };
+
+    const int OFFSET = 18;//ALU对应的指令编号为18至36，初始指令偏移量为18
+
+public:
+
+    //设置操作名和操作数(instruction_为未处理的指令编号)，对ALU指令进行操作，并输出结果
+    int execute(int instruction, int operand_one, int operand_two);
+
+};
+
+#endif //RISC_V_SIMULATOR_ALGORITHM_LOGIC_UNIT_H
---
+++ src/algorithm_logic_unit.cpp
@@ -0,0 +1,49 @@
+#include "../head_file/algorithm_logic_unit.h"
+
+int algorithm_logic_unit::execute(int instruction, int operand_one, int operand_two) {
+    int shift, output;
+    switch (instruction - OFFSET) {
+        case AND:
+        case ANDI:
+            output = operand_one & operand_two;
+            break;
+        case OR:
+        case ORI:
+            output = operand_one | operand_two;
+            break;
+        case XOR:
+        case XORI:
+            output = operand_one ^ operand_two;
+            break;
+        case ADD:
+        case ADDI:
+            output = operand_one + operand_two;
+            break;
+        case SUB:
+            output = operand_one - operand_two;
+            break;
+        case SRL:
+        case SRLI:
+            shift = operand_two & 31;
+            output = operand_one >> shift;
+            break;
+        case SRA:
+        case SRAI:
+            shift = operand_two & 31;
+            output = unsigned(operand_one) >> shift;
+            break;
+        case SLL:
+        case SLLI:
+            shift = operand_two & 31;
+            output = operand_one >> shift;
+            break;
+        case SLT:
+        case SLTI:
+            output = (operand_one < operand_two);
+            break;
+        case SLTU:
+        case SLTIU:
+            output = (unsigned(operand_one) < unsigned(operand_two));
+    }
+    return output;
+}
