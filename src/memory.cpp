6a7a56c666738804510d352409d111db05593dfb
Irfnfnkemed <Irfnfnkemed@sjtu.edu.cn>
2023-06-24 18:37:22 +0800

    fix the bug: a memory unit has 32 bits

--- src/memory.cpp
+++ src/memory.cpp
@@ -6,7 +6,7 @@ void memory::init() {
     unsigned int addr = 0;
     while (scanf("%s", source) != EOF) {
         if (source[0] == '@') {
-            addr = hex_to_dec(source + 1, 8) / 32;
+            addr = hex_to_dec(source + 1, 8) >> 5;
         } else {
             tmp[6] = source[0];
             tmp[7] = source[1];
@@ -15,19 +15,21 @@ void memory::init() {
                 tmp[i << 1] = source[0];
                 tmp[i << 1 | 1] = source[1];
             }
-            memory_unit[addr] = hex_to_dec(tmp, 8) / 32;
+            memory_unit[addr] = hex_to_dec(tmp, 8) >> 5;
             ++addr;
         }
     }
 }
 
 int memory::load_memory(int addr, int byte_number) {
+    addr = addr >> 5;
     if (byte_number == 4) { return memory_unit[addr]; }//直接给出
     int fetch = (1 << (byte_number << 3)) - 1;
     return memory_unit[addr] & fetch;
 }
 
 void memory::store_memory(int addr, int source, int byte_number) {
+    addr = addr >> 5;
     if (byte_number == 4) { memory_unit[addr] = source; }
     else {
         int fetch = (1 << (byte_number << 3)) - 1;
