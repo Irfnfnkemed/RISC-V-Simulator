0010c103878cda83d1be0898de0909455b578759
Irfnfnkemed <Irfnfnkemed@sjtu.edu.cn>
2023-06-25 18:54:43 +0800

    change the structure of L/S_Buffer

--- head_file/load_store_buffer.h
+++ head_file/load_store_buffer.h
@@ -2,49 +2,44 @@
 #define RISC_V_SIMULATOR_LOAD_STORE_BUFFER_H
 
 #include "memory.h"
-#include "algorithm_logic_unit.h"
-#include "tool.h"
 #include "reorder_buffer.h"
+#include "tool.h"
 
 class load_store_buffer {
 private:
-    enum INSTRUCTION {
-        LB, LH, LW, LBU, LHU, SB, SH, SW
-    };
-    struct load_store_unit {
-        int instruction;//load_store_buffer相关的指令编号(减去OFFSET)，空时为-1
-        int value_one, value_two;//两个相关寄存器的值。若有依赖关系，则存储寄存器编号
-        int depend_one = -1, depend_two = -1;//两个相关寄存器对前面指令的依赖关系，存储相应的寄存器编号，无依赖时默认为-1
-        int offset;//偏移量
-        int destination;//存指令的这一项是无用的
-        int tag;//读写指令在ROB中对应的tag
-        bool execute = false;//是否已经计算出地址
-    };
 
-    const int OFFSET = 10;//load_store_buffer对应的指令编号为10至17，初始指令偏移量为10
+    struct load_store_unit {
+        int instruction = -1;//load_store_buffer相关的指令编号，空时为-1
+        int value_one, value_two;//两个相关寄存器的值
+        int tag;//读写指令在ROB中对应的tag
+    };
 
     queue<load_store_unit, 64> buffer;//循环队列
+    queue<load_store_unit, 64> buffer_next;//缓冲区
     int clock_time = 0;
-    address_algorithm_logic_unit address_ALU;
     memory *Memory;
-    reorder_buffer *Reorder_buffer;
+    reorder_buffer *ROB;
+
+    //内存读写进程
+    void load_store_memory();
+
+    //设置下一个读写任务
+    void set_task();
 
 public:
 
     //初始化，关联相关模块
-    void init(memory *Memory_, reorder_buffer *Reorder_buffer_);
+    void init(memory *Memory_, reorder_buffer *ROB_);
 
-    //添加任务，instruction_为未处理的指令编号
-    void add_instruction(int instruction_, int value_one_, int value_two_,
-                         int depend_one_, int depend_two_,
-                         int offset, int destination_, int tag_);
+    //添加任务
+    void add_instruction(int instruction_, int value_one_, int value_two_, int tag_);
 
-    void execute_rising_edge();
+    //执行函数
+    void execute();
 
-    void execute_falling_edge();
+    //将缓冲区更新至当前状态
+    void flush();
 
-    //监听CDB
-    void response_CDB(int tag_, int reg_, int data_);
 };
 
 #endif //RISC_V_SIMULATOR_LOAD_STORE_BUFFER_H
--- src/load_store_buffer.cpp
+++ src/load_store_buffer.cpp
@@ -1,32 +1,11 @@
 #include "../head_file/load_store_buffer.h"
 
-void load_store_buffer::init(memory *Memory_, reorder_buffer *Reorder_buffer_) {
-    Memory = Memory_;
-    Reorder_buffer = Reorder_buffer_;
-}
 
-void load_store_buffer::add_instruction(int instruction_, int value_one_, int value_two_,
-                                        int depend_one_, int depend_two_,
-                                        int offset, int destination_, int tag_) {
-    buffer.push(load_store_unit{instruction_ - OFFSET, value_one_, value_two_,
-                                depend_one_, depend_two_,
-                                offset, destination_, tag_, false});
-}
-
-void load_store_buffer::execute_rising_edge() {
-    for (auto iter = buffer.begin(); iter != buffer.end(); ++iter) {
-        if (!iter->execute && iter->depend_one == -1 && iter->depend_two == -1) {//计算地址
-            iter->value_one = address_ALU.execute(iter->value_one, iter->offset);
-            iter->execute = true;
-        }
-    }
-}
-
-void load_store_buffer::execute_falling_edge() {
+void load_store_buffer::load_store_memory() {
     if (clock_time > 0) {//正常进行读写进程
         --clock_time;
         if (!clock_time) {//时钟数清零，表明某个内存读写操作已经完成
-            load_store_unit &task = buffer.front();
+            load_store_unit task = buffer_next.pop();
             switch (task.instruction) {
                 case LB:
                     task.value_two = sign_extend(Memory->load_memory(task.value_one, 1), 8);
@@ -52,22 +31,31 @@ void load_store_buffer::execute_falling_edge() {
                 case SW:
                     Memory->store_memory(task.value_one, task.value_two, 4);
             }
-            //////////////////////// Reorder_buffer->set_ready(tag_);
+            if (task.instruction >= LB && task.instruction <= LHU) {//将load指令返回ROB
+                ROB->set_ready(task.tag, task.value_two);
+            }
         }
     }
-    //设置下一个读写任务
-    if (!clock_time && !buffer.empty() && buffer.front().execute) { clock_time = 3; }
 }
 
+void load_store_buffer::set_task() {
+    if (!clock_time && !buffer.empty()) { clock_time = 3; }
+}
 
-void load_store_buffer::response_CDB(int tag_, int reg_, int data_) {
-    for (auto iter = buffer.begin(); iter != buffer.end(); ++iter) {
-        if (iter->depend_one == tag_) {
-            if (iter->value_one == reg_) { iter->value_one = data_; }
-            iter->depend_one = -1;
-        } else if (iter->depend_two == reg_) {
-            if (iter->depend_two == reg_) { iter->value_two = data_; }
-            iter->depend_two = -1;
-        }
-    }
+void load_store_buffer::init(memory *Memory_, reorder_buffer *ROB_) {
+    Memory = Memory_;
+    ROB = ROB_;
+}
+
+void load_store_buffer::add_instruction(int instruction_, int value_one_, int value_two_, int tag_) {
+    buffer_next.push(load_store_unit{instruction_, value_one_, value_two_, tag_});
+}
+
+void load_store_buffer::execute() {
+    load_store_memory();
+    set_task();
+}
+
+void load_store_buffer::flush() {
+    buffer = buffer_next;
 }
