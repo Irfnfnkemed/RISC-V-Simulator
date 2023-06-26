a646277eb641e0ba1c58e2b40bd3be7ca0ada88f
Irfnfnkemed <Irfnfnkemed@sjtu.edu.cn>
2023-06-26 10:22:03 +0800

    change the structure of RS module

--- head_file/reservation_staion.h
+++ head_file/reservation_staion.h
@@ -1,51 +1,55 @@
-#ifndef RISC_V_SIMULATOR_RESERVATION_STAION_H
-#define RISC_V_SIMULATOR_RESERVATION_STAION_H
+#ifndef RISC_V_SIMULATOR_RESERVATION_STATION_H
+#define RISC_V_SIMULATOR_RESERVATION_STATION_H
 
 #include "algorithm_logic_unit.h"
 #include "reorder_buffer.h"
+#include "load_store_buffer.h"
 #include "tool.h"
 
 class reservation_station {
 private:
 
-    enum INSTRUCTION {
-        ADDI, SLTI, SLTIU, XORI, ORI, ANDI, SLLI, SRLI, SRAI,
-        ADD, SUB, SLL, SLT, SLTU, XOR, SRL, SRA, OR, AND
-    };
-
-    const int OFFSET = 18;//reservation_station对应的指令编号为18至36，初始指令偏移量为18
-
     struct reservation_unit {
-        int instruction = -1;//指令编号(减去OFFSET)，为-1时表示该位置为空
+        int instr = -1;//指令编号，为-1时表示该位置为空
         int value_one, value_two;//两个寄存器(若存在)的值。若有依赖关系，则存储寄存器编号
         int depend_one = -1, depend_two = -1;//两个寄存器(若存在)对前面指令的依赖关系，存储依赖指令的tag，无依赖时默认为-1
-        int immediate;//存放立即数(或者运算结果)
-        int destination;//最终存放的目标寄存器编号(若存在)
+        int dest;//目标寄存器编号
+        int imd;//存放立即数(或者运算结果)
         int tag;//指令在ROB中的tag
-        int status = 0;//0表未计算，1表执行但未提交，2表已经提交至ROB
+        bool ready = false;
     };
 
-    algorithm_logic_unit ALU;
-    reorder_buffer *Reorder_buffer;
+    all_ALU ALU;
+    reorder_buffer *ROB;
+    load_store_buffer *LSB;
 
     reservation_unit buffer[16];
+    reservation_unit buffer_next[16];//缓冲区
+
+    //将可执行的放入ALU中进行运算，将结果返回ROB/LSB，并在RS中删去
+    void calculate();
+
+    //将算完的任务返回到ROB或LSB(load指令)
+    void return_to_ROB_or_LSB(int instr_, int tag_, int data_);
+
+    //删去标签为tag_对应的寄存器依赖关系，并修改其值
+    void flush_depend(int tag_, int data_);
 
 public:
 
-    void init(reorder_buffer *Reorder_buffer_);
+    //初始化
+    void init(reorder_buffer *ROB_, load_store_buffer *LSB_);
 
-    //添加任务，instruction_为未处理的指令编号
-    void add_instruction(int instruction_, int value_one_, int value_two_,
-                         int depend_one_, int depend_two_,
-                         int immediate_, int destination_, int tag_);
+    //添加任务
+    void add_instruction(int instr_, int value_one_, int value_two_,
+                         int depend_one_, int depend_two_, int imd_, int tag_);
 
-    void execute_rising_edge();
+    //执行函数
+    void execute();
 
-    void execute_falling_edge();
-
-    //监听CDB。删去标签为tag_的指令，并修改reg_对应的值
-    void response_CDB(int tag_, int reg_, int data_);
+    //刷新
+    void flush();
 
 };
 
-#endif //RISC_V_SIMULATOR_RESERVATION_STAION_H
+#endif //RISC_V_SIMULATOR_RESERVATION_STATION_H
--- src/reservation_staion.cpp
+++ src/reservation_staion.cpp
@@ -1,56 +1,66 @@
 #include "../head_file/reservation_staion.h"
 
-void reservation_station::init(reorder_buffer *Reorder_buffer_) {
-    Reorder_buffer = Reorder_buffer_;
-}
-
-void reservation_station::add_instruction(int instruction_, int value_one_, int value_two_,
-                                          int depend_one_, int depend_two_,
-                                          int immediate_, int destination_, int tag_) {
-    int index = 0;
-    while (buffer[index].status != 0) { ++index; }
-    buffer[index] = reservation_unit{instruction_ - OFFSET, value_one_, value_two_,
-                                     depend_one_, depend_two_, immediate_, destination_, tag_, 0};
-}
-
-void reservation_station::execute_rising_edge() {
+void reservation_station::calculate() {
     for (int i = 0; i < 16; ++i) {
-        if (buffer[i].instruction >= 0 && buffer[i].status == 0) {
-            if (buffer[i].instruction < 9) {
-                if (buffer[i].depend_one == -1) {
-                    buffer[i].immediate = ALU.execute(buffer[i].instruction,
-                                                      buffer[i].value_one, buffer[i].immediate);
-                    buffer[i].status = 1;
+        if (buffer[i].instr >= 0 && !buffer[i].ready) {
+            if (buffer[i].instr == JALR ||
+                (buffer[i].instr >= LB && buffer[i].instr <= LHU) ||
+                (buffer[i].instr >= ADDI && buffer[i].instr <= SRAI)) {//有一个立即数的操作
+                if (ALU.is_free(buffer[i].instr) && buffer[i].depend_one == -1) {
+                    buffer_next[i].imd = ALU.execute(buffer[i].instr, buffer[i].value_one, buffer[i].imd);
+                    buffer_next[i].ready = true;
+                    return_to_ROB_or_LSB(buffer[i].instr, buffer[i].tag, buffer_next[i].imd);
+                    flush_depend(buffer[i].instr, buffer_next[i].imd);//更新依赖关系
+                    buffer_next[i].instr = -1;//删去
                 }
-            } else {
-                if (buffer[i].depend_one == -1 && buffer[i].depend_one == -1) {
-                    buffer[i].immediate = ALU.execute(buffer[i].instruction,
-                                                      buffer[i].value_one, buffer[i].value_two);
-                    buffer[i].status = 1;
+            } else {//两个寄存器的操作
+                if (ALU.is_free(buffer[i].instr) && buffer[i].depend_one == -1 && buffer[i].depend_two == -1) {
+                    buffer_next[i].imd = ALU.execute(buffer[i].instr, buffer[i].value_one, buffer[i].value_two);
+                    buffer_next[i].ready = true;
+                    return_to_ROB_or_LSB(buffer[i].instr, buffer[i].tag, buffer_next[i].imd);
+                    flush_depend(buffer[i].instr, buffer_next[i].imd);//更新依赖关系
+                    buffer_next[i].instr = -1;//删去
                 }
             }
         }
     }
 }
 
-void reservation_station::execute_falling_edge() {
+void reservation_station::return_to_ROB_or_LSB(int instr_, int tag_, int data_) {
+    if (instr_ >= LB && instr_ <= LHU) { LSB->add_instruction(instr_, data_, -1, tag_); }
+    else { ROB->set_ready(tag_, data_); }
+}
+
+void reservation_station::flush_depend(int tag_, int data_) {
     for (int i = 0; i < 16; ++i) {
-        if (buffer[i].status == 1) {
-            //////////////////////// Reorder_buffer->set_ready(tag_);
+        if (buffer[i].instr != -1) {
+            if (buffer[i].depend_one == tag_) {
+                buffer_next[i].value_one = data_;
+                buffer_next[i].depend_one = -1;
+            } else if (buffer[i].depend_two == tag_) {
+                buffer_next[i].value_two = data_;
+                buffer_next[i].depend_two = -1;
+            }
         }
     }
 }
 
-void reservation_station::response_CDB(int tag_, int reg_, int data_) {
-    for (int i = 0; i < 16; ++i) {
-        if (buffer[i].tag == tag_) {
-            buffer[i].instruction = -1; //设为空闲
-        } else if (buffer[i].depend_one == tag_) {
-            if (buffer[i].value_one == reg_) { buffer[i].value_one = data_; }
-            buffer[i].depend_one = -1;
-        } else if (buffer[i].depend_two == tag_) {
-            if (buffer[i].value_two == reg_) { buffer[i].value_two = data_; }
-            buffer[i].depend_two = -1;
-        }
-    }
+void reservation_station::init(reorder_buffer *ROB_, load_store_buffer *LSB_) {
+    ROB = ROB_;
+    LSB = LSB_;
+}
+
+void reservation_station::add_instruction(int instr_, int value_one_, int value_two_,
+                                          int depend_one_, int depend_two_, int imd_, int tag_) {
+    int index = 0;
+    while (buffer[index].instr != -1) { ++index; }
+    buffer_next[index] = reservation_unit{instr_, value_one_, value_two_,
+                                          depend_one_, depend_two_, imd_, tag_, false};
+}
+
+void reservation_station::execute() { calculate(); }
+
+
+void reservation_station::flush() {
+    for (int i = 0; i < 16; ++i) { buffer[i] = buffer_next[i]; }
 }
