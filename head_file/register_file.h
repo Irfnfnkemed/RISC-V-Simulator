0eff60bcb6efb9c3c515812eb3b8b7e59fb9c25d
Irfnfnkemed <Irfnfnkemed@sjtu.edu.cn>
2023-06-26 11:31:43 +0800

    finish the register_file module

--- head_file/register_file.h
+++ head_file/register_file.h
@@ -4,19 +4,30 @@
 class register_file {
 private:
 
-    int register_unit[32];
+    int register_unit[32];//寄存器的值
     int depend[32];//存储依赖的指令在ROB中的tag，无关系时为-1
+    int register_unit_next[32];//缓冲区
+    int depend_next[32];//缓冲区
 
 public:
 
+    //初始化
     void init();
 
+    //获得依赖关系
     int get_depend(int reg_);
 
+    //获得寄存器的值
     int get_value(int reg_);
 
-    //监听CDB
-    void response_CDB(int tag_, int reg_, int data_);
+    //指令完成提交后，删除相关标记，并更新其值
+    void flush_tag(int tag_, int data_);
+
+    //给reg_寄存器存标记
+    void set_tag(int tag_, int reg_);
+
+    //刷新
+    void flush();
 
 };
 
--- src/register_file.cpp
+++ src/register_file.cpp
@@ -1,7 +1,7 @@
 #include "../head_file/register_file.h"
 
 void register_file::init() {
-    for (int i = 0; i < 32; ++i) { depend[i] = -1; }
+    for (int i = 0; i < 32; ++i) { depend[i] = depend_next[i] = -1; }
 }
 
 int register_file::get_depend(int reg_) { return depend[reg_]; }
@@ -11,11 +11,22 @@ int register_file::get_value(int reg_) {
     return reg_;
 }
 
-void register_file::response_CDB(int tag_, int reg_, int data_) {
+void register_file::flush_tag(int tag_, int data_) {
     for (int i = 0; i < 32; ++i) {
         if (depend[i] == tag_) {
-            if (i == reg_) { register_unit[i] = data_; }
-            depend[i] = -1;
+            register_unit_next[i] = data_;
+            depend_next[i] = -1;
         }
+    }
+}
+
+void register_file::set_tag(int tag_, int reg_) {
+    depend_next[reg_] = tag_;
+}
+
+void register_file::flush() {
+    for (int i = 0; i < 32; ++i) {
+        register_unit[i] = register_unit_next[i];
+        depend[i] = depend_next[i];
     }
 }
