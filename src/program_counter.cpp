579d5ad65f6bcddfd8f63573c67cb7902d732f22
Irfnfnkemed <Irfnfnkemed@sjtu.edu.cn>
2023-06-28 09:43:22 +0800

    fix the bug: update the stop status

--- src/program_counter.cpp
+++ src/program_counter.cpp
@@ -17,13 +17,13 @@ int program_counter::get_pc() { return pc; }
 void program_counter::set_stop(bool stop_) { stop_next = stop_; }
 
 void program_counter::flush() {
+    stop = stop_next;
     if (!stop) {
         if (begin) { pc = offset; }
         else { pc += offset; }
     }
     offset = 4;
     begin = false;
-    stop = stop_next;
 }
 
 void program_counter::clear() {
